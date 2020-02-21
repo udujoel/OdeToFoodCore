96a4968eb016c92d81de8af6a8d8baaac8fb9cd8
Joell <amamihechi.udu@decagon.dev>
2020-02-21 03:37:50 +0100

    initial commit

--- OdeToFoodCore/Pages/Restaurants/List.cshtml.cs
+++ OdeToFoodCore/Pages/Restaurants/List.cshtml.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace OdeToFoodCore
 {
     public class ListModel : PageModel
     {
+        public string Message { get; set; }
         public void OnGet()
         {
+            Message = "Hello world";
 
         }
     }
