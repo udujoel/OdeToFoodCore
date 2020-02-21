4bfa319846ba6591cbe08813f177b1e427b309ff
Joell <amamihechi.udu@decagon.dev>
2020-02-21 03:45:28 +0100

    setting and reading from app config file

--- OdeToFoodCore/Pages/Restaurants/List.cshtml.cs
+++ OdeToFoodCore/Pages/Restaurants/List.cshtml.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFoodCore
 {
     public class ListModel : PageModel
     {
         public string Message { get; set; }
+
+        public ListModel(IConfiguration iConfiguration)
+        {
+            Message = iConfiguration["message"];
+        }
         public void OnGet()
         {
-            Message = "Hello world";
+            //            Message = "Hello world";
 
         }
     }
