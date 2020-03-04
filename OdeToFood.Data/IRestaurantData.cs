1d9cf3b2567969ca638923cc9bd0612358689f53
Joell <amamihechi.udu@decagon.dev>
2020-03-04 03:00:03 +0100

    inmemory restaurant data

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using OdeToFood.Core;
 
@@ -11,9 +12,26 @@ namespace OdeToFood.Data
     }
 
 
-    public class Getall()
+    public class InMemoryRestaurantData : IRestaurantData
     {
+        private List<Restaurant> restaurants;
 
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant(){Id = 1, Cuisine = CuisineType.Mexican, Location = "NY", Name = "Martin Sixers"},
+                new Restaurant(){Id = 2, Cuisine = CuisineType.Nigerian, Location = "Abuja", Name = "Crunchies"},
+                new Restaurant(){Id = 3, Cuisine = CuisineType.Italian, Location = "Toll troll", Name = "Uncle Ben"},
+                new Restaurant(){Id = 4, Cuisine = CuisineType.Indian, Location = "Maryland", Name = "Scott Fries"},
+                new Restaurant(){Id = 5, Cuisine = CuisineType.Nigerian, Location = "LG", Name = "Red Buffalos"}
+            };
+        }
+        
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(x=>x.Name);
+
+        }
     }
-
 }
