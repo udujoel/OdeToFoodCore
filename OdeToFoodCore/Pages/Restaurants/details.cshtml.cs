d5cde80eba30799a30cf947010e6ca274f56e27b
Joell <amamihechi.udu@decagon.dev>
2020-03-04 11:42:55 +0100

    passing url data using asp-route

--- OdeToFoodCore/Pages/Restaurants/details.cshtml.cs
+++ OdeToFoodCore/Pages/Restaurants/details.cshtml.cs
@@ -4,14 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
 
 namespace OdeToFoodCore
 {
     public class detailsModel : PageModel
     {
-        public void OnGet()
+        public Restaurant Restaurant { get; set; }
+        public void OnGet(int restaurantid)
         {
-
+            Restaurant = new Restaurant();
+            Restaurant.Id = restaurantid;
         }
     }
 }
